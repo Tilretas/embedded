742b3e1027caebc2add6714e2e5a4c5ee16d1360
Tilretas <badampeter@gmail.com>
2024-03-19 20:46:15 +0100

    updated

--- Ex6/Core/Src/main.c
+++ Ex6/Core/Src/main.c
@@ -15,16 +15,33 @@
   *
   ******************************************************************************
   */
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
 /* USER CODE END Header */
+
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "i2c.h"
-#include "usart.h"
-#include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include <string.h>
+#include <stdio.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -43,13 +60,20 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
+UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN PV */
-
+static const uint8_t TMP102_ADDR = 0x48 << 1; // Use 8-bit address
+static const uint8_t REG_TEMP = 0x00;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_I2C1_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -66,8 +90,12 @@ void SystemClock_Config(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
+  HAL_StatusTypeDef ret;
+  uint8_t buf[12];
+  int16_t val;
+  float temp_c;
   /* USER CODE END 1 */
+
 
   /* MCU Configuration--------------------------------------------------------*/
 
@@ -97,6 +125,46 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+
+    // Tell TMP102 that we want to read from the temperature register
+    buf[0] = REG_TEMP;
+    ret = HAL_I2C_Master_Transmit(&hi2c1, TMP102_ADDR, buf, 1, HAL_MAX_DELAY);
+    if ( ret != HAL_OK ) {
+      strcpy((char*)buf, "Error Tx\r\n");
+    } else {
+
+      // Read 2 bytes from the temperature register
+      ret = HAL_I2C_Master_Receive(&hi2c1, TMP102_ADDR, buf, 2, HAL_MAX_DELAY);
+      if ( ret != HAL_OK ) {
+        strcpy((char*)buf, "Error Rx\r\n");
+      } else {
+
+        //Combine the bytes
+        val = ((int16_t)buf[0] << 4) | (buf[1] >> 4);
+
+        // Convert to 2's complement, since temperature can be negative
+        if ( val > 0x7FF ) {
+          val |= 0xF000;
+        }
+
+        // Convert to float temperature value (Celsius)
+        temp_c = val * 0.0625;
+
+        // Convert temperature to decimal format
+        temp_c *= 100;
+        sprintf((char*)buf,
+              "%u.%u C\r\n",
+              ((unsigned int)temp_c / 100),
+              ((unsigned int)temp_c % 100));
+      }
+    }
+
+    // Send out buffer (temperature or error message)
+    HAL_UART_Transmit(&huart2, buf, strlen((char*)buf), HAL_MAX_DELAY);
+
+    // Wait
+    HAL_Delay(500);
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
